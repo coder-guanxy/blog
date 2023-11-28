97e33c450a726672a2218ec2414ae93ee71d2be7
guanxiangyu <guanxiangyu@csii.com.cn>
2023-11-28 10:44:13 +0800

    feat: 初始化 blog

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    title: '1Hello VitePress',
+    description: 'Just playing around.',
+  base: '/blog/',
+    
+  themeConfig: {
+      docFooter: {
+        prev: 'Get Started | Markdown',
+        next: 'Proxima pagina'
+      },
+      
+      footer: {
+        message: 'Released under the MIT License.',
+        copyright: 'Copyright © 2019-present Evan You'
+      }
+    }
+  }
